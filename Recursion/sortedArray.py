3a4531af4e5d1c7a2eb6f60cf4060ce769c95921
Ujjwaltewatia <35035764+Ujjwaltewatia@users.noreply.github.com>
2022-08-04 18:26:08 +0530

    Create sortedArray.py

---
+++ Recursion/sortedArray.py
@@ -0,0 +1,13 @@
+arr=[1,2,3,4,5,44,6]
+
+def sorted(arr,index=0):
+    
+    if index==len(arr)-1:
+        return True
+    
+    if arr[index]>arr[index+1]:
+        return False
+    index+=1
+    return sorted(arr,index)
+
+print(sorted(arr))
